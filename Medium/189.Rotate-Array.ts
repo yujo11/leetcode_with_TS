8996b59752fd0118d2d3836c59336b3f9ad72624
yujo <yujo.dev@gmail.com>
2022-02-27 11:23:29 +0900

    feat: 189 - Array

---
+++ Medium/189.Rotate-Array.ts
@@ -0,0 +1,6 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], k: number): void {
+  nums.push(...nums.splice(0, nums.length - (k % nums.length)));
+}
