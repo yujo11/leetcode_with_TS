7941abf4ce893286373586decd2999343a49f8df
yujo <yujo.dev@gmail.com>
2022-02-28 20:28:06 +0900

    feat: Linked List Type Definition

--- Easy/867.Middle-of-the-Linked-List.ts
+++ Easy/867.Middle-of-the-Linked-List.ts
@@ -1,12 +1,3 @@
-class ListNode {
-  val: number;
-  next: ListNode | null;
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-  }
-}
-
 function middleNode(head: ListNode | null): ListNode | null {
   let [slow, fast] = [head, head];
 
