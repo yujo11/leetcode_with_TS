7cb830339480ff5d3a0f7452794426c24b87a2fb
yujo <yujo.dev@gmail.com>
2022-05-29 14:51:59 +0900

    feat: Roteate Image

---
+++ Top-interview-question/Array/Rotate-Image.ts
@@ -0,0 +1,15 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix: number[][]): void {
+  //   const n = matrix.length
+  //   for (let i = 0; i < n / 2; i++) {
+  //     for (let j = i; j < n - i - 1; j++) {
+  //       const temp = matrix[i][j]
+  //       matrix[i][j] = matrix[n - 1 - j][i]
+  //       matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+  //       matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+  //       matrix[j][n - 1 - i] = temp
+  //     }
+  //   }
+}
