21b21abb69a18c39aca570a0e7c26a73e610e29b
조윤호 <yujo.dev@gmail.com>
2022-10-02 15:20:34 +0900

    feat: Delete Node in a Linked List

---
+++ Top-interview-questions/Easy/Linkend-List/Delete-Node-in-a-Linked-List.ts
@@ -0,0 +1,20 @@
+export {}
+
+class ListNode {
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+  }
+}
+
+/**
+ Do not return anything, modify it in-place instead.
+ */
+function deleteNode(root: ListNode | null): void {
+  if (root === null || root.next === null) return
+
+  root.val = root.next.val
+  root.next = root.next.next
+}
