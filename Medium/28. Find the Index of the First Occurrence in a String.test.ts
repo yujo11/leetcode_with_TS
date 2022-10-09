d1ae6d555f9180b64e2b192b6d6edb513591bf53
조윤호 <yujo.dev@gmail.com>
2022-10-09 15:41:53 +0900

    kmp solution 추가

--- Medium/28. Find the Index of the First Occurrence in a String.test.ts
+++ Medium/28. Find the Index of the First Occurrence in a String.test.ts
@@ -69,45 +69,84 @@ export {}
  * Solution 6
  */
 
-function strStr(haystack: string, needle: string): number {
-  if (needle.length > haystack.length) {
-    return -1
-  }
+// function strStr(haystack: string, needle: string): number {
+//   if (needle.length > haystack.length) {
+//     return -1
+//   }
 
-  if (!needle) {
-    return 0
-  }
+//   if (!needle) {
+//     return 0
+//   }
 
-  let R = 256
-  let Q = 999991
-  let M = needle.length
-  let RM = 1
+//   let R = 256
+//   let Q = 999991
+//   let M = needle.length
+//   let RM = 1
 
-  for (let a = 1; a <= M - 1; a++) {
-    RM = (R * RM) % Q
-  }
+//   for (let a = 1; a <= M - 1; a++) {
+//     RM = (R * RM) % Q
+//   }
 
-  let phash = hash(needle, M, Q, R)
-  let hashy = hash(haystack, M, Q, R)
-  if (phash === hashy) return 0
+//   let phash = hash(needle, M, Q, R)
+//   let hashy = hash(haystack, M, Q, R)
+//   if (phash === hashy) return 0
 
-  for (let i = 1; i + M - 1 <= haystack.length - 1; i++) {
-    let nextHash = (hashy + Q - ((haystack.charCodeAt(i - 1) * RM) % Q)) % Q
-    nextHash = (((nextHash * R) % Q) + haystack.charCodeAt(i + M - 1)) % Q
-    if (nextHash === phash) {
-      return i
+//   for (let i = 1; i + M - 1 <= haystack.length - 1; i++) {
+//     let nextHash = (hashy + Q - ((haystack.charCodeAt(i - 1) * RM) % Q)) % Q
+//     nextHash = (((nextHash * R) % Q) + haystack.charCodeAt(i + M - 1)) % Q
+//     if (nextHash === phash) {
+//       return i
+//     }
+//     hashy = nextHash
+//   }
+//   return -1
+// }
+
+// let hash = function (str: string, len: number, Q: number, R: number) {
+//   let h = 0
+//   for (let i = 0; i < len; i++) {
+//     h = (h * R + str.charCodeAt(i)) % Q
+//   }
+//   return h
+// }
+
+function strStr(haystack: string, needle: string) {
+  if (needle === '') return 0
+
+  // define lps array
+  const lps = new Array(needle.length).fill(0)
+  let prevLPS = 0
+  let i = 1
+  while (i < needle.length) {
+    if (needle[i] === needle[prevLPS]) {
+      lps[i] = prevLPS + 1
+      prevLPS += 1
+      i += 1
+    } else if (prevLPS === 0) {
+      lps[i] = 0
+      i += 1
+    } else {
+      prevLPS = lps[prevLPS - 1] // // cire KMP
     }
-    hashy = nextHash
+  }
+
+  // use lps to get res
+  i = 0 // ptr for haystack
+  let j = 0 // ptr for needle
+  while (i < haystack.length) {
+    if (haystack[i] === needle[j]) {
+      i++
+      j++
+    } else {
+      if (j === 0) {
+        i++
+      } else {
+        j = lps[j - 1] // cire KMP
+      }
+    }
+    if (j === needle.length) return i - needle.length
   }
   return -1
-}
-
-let hash = function (str: string, len: number, Q: number, R: number) {
-  let h = 0
-  for (let i = 0; i < len; i++) {
-    h = (h * R + str.charCodeAt(i)) % Q
-  }
-  return h
 }
 
 describe('strStr', () => {
