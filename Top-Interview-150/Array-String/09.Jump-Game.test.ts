9a3328639338a1073990b526f1443d96648c7211
JO YUN HO <yujo.dev@gmail.com>
2024-02-05 23:25:28 +0900

    Jump Game Youtube Solution 추가

--- Top-Interview-150/Array-String/09.Jump-Game.test.ts
+++ Top-Interview-150/Array-String/09.Jump-Game.test.ts
@@ -1,67 +1,84 @@
 /**
  * My Solution
  */
-function canJump1(nums: number[]): boolean {
-  let position = nums[0]
+// function canJump(nums: number[]): boolean {
+//   let position = nums[0]
 
-  for (let i = 1; i < nums.length; i++) {
-    if (position < i) {
-      return false
+//   for (let i = 1; i < nums.length; i++) {
+//     if (position < i) {
+//       return false
+//     }
+
+//     position = Math.max(position, nums[i] + i)
+//   }
+
+//   return true
+// }
+
+/**
+ * Youtube Solution
+ * - https://www.youtube.com/watch?v=Zb4eRjuPHbM
+ */
+
+function canJump(nums: number[]): boolean {
+  let lastPosition = nums.length - 1
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (i + nums[i] >= lastPosition) {
+      lastPosition = i
     }
-
-    position = Math.max(position, nums[i] + i)
   }
 
-  return true
+  return lastPosition === 0
 }
 
 /**
  * LeetCode Solution 01. Backtracking
  */
-function canJumpFromPosition(position: number, nums: number[]): boolean {
-  if (position === nums.length - 1) {
-    return true
-  }
+// function canJumpFromPosition(position: number, nums: number[]): boolean {
+//   if (position === nums.length - 1) {
+//     return true
+//   }
 
-  const furthestJump = Math.min(position + nums[position], nums.length - 1)
-  for (let nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
-    if (canJumpFromPosition(nextPosition, nums)) {
-      return true
-    }
-  }
+//   const furthestJump = Math.min(position + nums[position], nums.length - 1)
+//   for (let nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+//     if (canJumpFromPosition(nextPosition, nums)) {
+//       return true
+//     }
+//   }
 
-  return false
-}
+//   return false
+// }
 
-function canJump2(nums: number[]): boolean {
-  return canJumpFromPosition(0, nums)
-}
+// function canJump(nums: number[]): boolean {
+//   return canJumpFromPosition(0, nums)
+// }
 
 /**
  * LeetCode Solution 02. Dynamic Programming Top-Down
  */
-function canJumpFromPosition2(position: number, nums: number[], memo: number[]): boolean {
-  if (memo[position] !== 0) {
-    return memo[position] === 1
-  }
+// function canJumpFromPosition(position: number, nums: number[], memo: number[]): boolean {
+//   if (memo[position] !== 0) {
+//     return memo[position] === 1
+//   }
 
-  const furthestJump = Math.min(position + nums[position], nums.length - 1)
-  for (let nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
-    if (canJumpFromPosition2(nextPosition, nums, memo)) {
-      memo[position] = 1
-      return true
-    }
-  }
+//   const furthestJump = Math.min(position + nums[position], nums.length - 1)
+//   for (let nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+//     if (canJumpFromPosition(nextPosition, nums, memo)) {
+//       memo[position] = 1
+//       return true
+//     }
+//   }
 
-  memo[position] = -1
-  return false
-}
+//   memo[position] = -1
+//   return false
+// }
 
-function canJump(nums: number[]): boolean {
-  const memo = new Array(nums.length).fill(0)
-  memo[nums.length - 1] = 1
-  return canJumpFromPosition2(0, nums, memo)
-}
+// function canJump(nums: number[]): boolean {
+//   const memo = new Array(nums.length).fill(0)
+//   memo[nums.length - 1] = 1
+//   return canJumpFromPosition(0, nums, memo)
+// }
 
 describe('Jump Game', () => {
   test('case 1', () => {
